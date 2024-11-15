5ea4f4aeced44260622c25d91008cf4a42127424
Ghazy Fadhal Ramadhan <ramadhan.ghazyfadhal@gmail.com>
2024-11-15 22:40:55 +0700

    menyicil class Gudang dan Manager

--- Gudang.java
+++ Gudang.java
@@ -1,3 +1,32 @@
 public class Gudang {
-    
+    private String IDGudang;
+    private String namaG;
+    private double kapasitasMax;
+    //Stok
+    private ArrayList <StokMakanan>;
+    private String IDStok;
+    private String namaS;
+    private double jumlahS;
+    private String categoryFood;
+    private Manager picManager;
+    private Ranger picRanger;
+
+    public Gudang(String IDGudang, String namaG, double kapasitasMax){}
+
+    public void setPicRanger(Ranger picRanger, ArrayList <StokMakanan>) {}
+
+    public void setPicManager(Manager picManager, Gudang newGudang){}
+
+    public void addStokMakanan(String IDStok, String namaS, double jumlahS, String categoryFood) {}
+
+    public void updateStok(ArrayList<StokMakanan> stok) {}
+
+    public void deleteStok(ArrayList<StokMakanan> stok) {}
+
+    public void cekListGudang();
+
+    public void cekListStok();
+
+    public void statusGudang();
+
 }
--- Manager.java
+++ Manager.java
@@ -1,4 +1,65 @@
 public class Manager extends Employee implements JobManager {
-    private String nama;
+    private Manager picManager;
+    private ArrayList() ;
+    private PenghuniKandang ukuran;
+    private PenghuniKandang tipe;
+    private PenghuniKandang spesial;
+    
+    public Manager(String idEmployee, String nama, int usia, String alamat, String noTelepon) {
+        super(idEmployee, nama, usia, alamat, noTelepon);
+    }
+
+    public void payEmployees(int idEmployee, int amount, String date){
+
+    }
+
+    public void addHewan(String nama, int umur, String jenisKelamin, int jumlah, String tipeMakan, double berat, PenghuniKandang ukuran, PenghuniKandang tipe, PenghuniKandang spesial) {
+
+    }
+
+    public Ranger getRanger(){
+        return nama;
+    }
+
+    public void updateKandang(String ukuran) {
+
+    }
+
+    public void deleteKandang() {
+
+    }
+
+    public void deleteHewan(String nama) {
+
+    }
+
+    public void createGudang() {
+
+    }
+
+    public void updateGudang(double kapasitasMax) {
+
+    }
+
+    public void showListHewan() {
+
+    }
+
+    public void showListStok() {
+
+    }
+
+    public void showAnakBuah() {
+
+    }
+
+    public void cekListGudang() {
+
+    }
+
+    public void getJobDesk() {
+
+    }
+
 
 }
