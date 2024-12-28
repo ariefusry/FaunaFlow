c864a6c60825d4d44173192612deeb4f1f2bea95
aiursmint <149082484+aiursmint@users.noreply.github.com>
2024-12-28 20:16:12 +0700

    Create Kandang.java

---
+++ FaunaFlowGG/src/main/java/com/mycompany/faunaflowgg/Kandang.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.faunaflowgg;
+
+/**
+ *
+ * @author LENOVO
+ */
+public abstract class Kandang {
+    protected double ukuran;
+    protected String tipe;
+    protected String spesial;
+
+    public abstract void ukuranKandang();
+    public abstract void tipeKandang();
+    public abstract void spesialKandang();
+}
